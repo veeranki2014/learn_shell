3a95bc058d92b98777f31779dcc3b09fecc11087
veeranki2014 <veeranki2014@gmail.com>
2023-06-16 15:33:20 -0500

    test

---
+++ 02-print.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "\e[30m Hello world \e[0m"
+echo -e "\e[31m Hello world \e[0m"
+echo -e "\e[32m Hello world \e[0m"
+echo -e "\e[33m Hello world \e[0m"
+echo -e "\e[34m Hello world \e[0m"
+echo -e "\e[35m Hello world \e[0m"
