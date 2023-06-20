61113018af3dfb09236cc599d755f5174e5c6c18
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 23:19:25 -0500

    roboshop_setup_shell

--- 07-while.sh
+++ 07-while.sh
@@ -5,3 +5,7 @@ while [ $a -ge 0 ]; do
   a=$(($a-1))
   sleep 1
 done
+
+for componet in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch; do
+  echo creating roboshop component - $component
+done
