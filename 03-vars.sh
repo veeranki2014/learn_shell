cf47a56253991940d0056e22abe48c56bc40c7d3
veeranki2014 <veeranki2014@gmail.com>
2023-06-19 21:47:27 -0500

    roboshop_setup_shell

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,3 +3,13 @@ a=10
 echo value of a is $adollars
 
 echo value of a is ${a}dollars
+
+#special variables are used for inputs
+# Special variables are $0,$1,$N,$#,$@,$?,$$
+echo $0
+echo $1
+echo $2
+echo $@
+echo $#
+echo $$
+echo $?
--- 04-funtions.sh
+++ 04-funtions.sh
@@ -4,13 +4,5 @@ print_hello(){
 
 print_hello
 
-#special variables are used for inputs
-# Special variables are $0,$1,$N,$#,$@,$?,$$
-echo $0
-echo $1
-echo $2
-echo $@
-echo $#
-echo $$
-echo $?
+
 
